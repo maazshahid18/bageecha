64c8c8e1ce30f57aa640b0832cad69853abb4814
Maaz <maaz.shahid@91trucks.com>
2026-07-04 16:56:47 +0530

    Add Instagram post CbXip7KB84p to gallery feed

--- src/data/instagram-posts.ts
+++ src/data/instagram-posts.ts
@@ -1,4 +1,5 @@
 export const instagramPosts: string[] = [
+  "https://www.instagram.com/p/CbXip7KB84p/",
   "https://www.instagram.com/reel/DV2Sc8bE-5W/",
   "https://www.instagram.com/reel/DOz8nCnEljt/",
   "https://www.instagram.com/reel/CjbEeu1hcNI/",
